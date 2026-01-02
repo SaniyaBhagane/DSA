0dec0eb5c32f9824ab0c3f1aec3cd4a638f94a08
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-01-02 23:28:23 +0530

    optimized

--- array/onboarding.py
+++ array/onboarding.py
@@ -4,15 +4,28 @@
 # https://leetcode.com/problems/max-consecutive-ones/
 
 # Approach => In a loop keep count of 1s, then reset count when 0 is encountered. Keep track of max count in res variable. 
-class Solution:
-    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        count = 0
-        res = 0
-        for i in nums:
-            if i == 1:
-                count += 1
-                res = max(count, res)
-            else:
-                count = 0
-        return res
-        
+
+# class Solution:
+#     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+#         count = 0
+#         res = 0
+#         for i in nums:
+#             if i == 1:
+#                 count += 1
+#                 res = max(count, res)
+#             else:
+#                 count = 0
+#         return res
+# => The above approach can be optimized slightly by updating maxCount only when 0 is encountered.
+
+# class Solution:
+#     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+#         count = 0
+#         maxCount = 0
+#         for i in nums:
+#             if i == 1:
+#                 count += 1
+#             else:
+#                 maxCount = max(maxCount, count)
+#                 count = 0
+#         return max(maxCount, count)        
