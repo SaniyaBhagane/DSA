4ff04b798ced1fb4fbdc90cce7142b18075eb20b
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-01-02 20:39:10 +0530

    .

--- array/arrays&hashing.py
+++ array/arrays&hashing.py
@@ -69,5 +69,73 @@
 # -------------------------------------------------------------------------------------------------
 # 242: VALID ANAGRAM
 
+# BRUTEFIRCE: SORTING
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         sorted_s = sorted(s)
+#         sorted_t = sorted(t)
+#         if sorted_s == sorted_t: 
+#             return True
+#         return False
+
+# APPROACH 2: Hash Table
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         count = defaultdict(int)
+#         # Count the frequency of characters in string s
+#         for x in s:
+#             count[x] += 1
+#         # Decrement the frequency of characters in string t
+#         for x in t:
+#             count[x] -= 1        
+#         # Check if any character has non-zero frequency
+#         for val in count.values():
+#             if val != 0:
+#                 return False       
+#         return True
+
+# APPROACH 3: Hash Table (26-length Array Counting)
+# OPTIMAL
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         count = [0] * 26        
+#         # Count the frequency of characters in string s
+#         for x in s:
+#             count[ord(x) - ord('a')] += 1
+#         # Decrement the frequency of characters in string t
+#         for x in t:
+#             count[ord(x) - ord('a')] -= 1        
+#         # Check if any character has non-zero frequency
+#         for val in count:
+#             if val != 0:
+#                 return False        
+#         return True
+    
+    
+# -------------------------------------------------------------------------------------------------
+# 1. TWO SUM
+
+# BRUTEFORCE : Two pointers
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+#         return []  # Fallback (problem guarantees a solution)
+
+# APPROACH 2: One-pass Hash Table
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         hashmap={}
+#         for i, num in enumerate(nums):
+#             value=target-num
+#             if value in hashmap:
+#                 return[hashmap[value], i]
+#             hashmap[num]=i
+
+
+# -------------------------------------------------------------------------------------------------
+# 14. LONGEST COMMON PREFIX
 
 # -------------------------------------------------------------------------------------------------
